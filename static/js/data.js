a483dad684e0e1cf0a7073a067fba168fbea2453
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-02-08 10:08:56 -0500

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -2,8 +2,8 @@ var data = [
   {
     datetime: "1/1/2010",
     city: "benton",
-    country: "us",
     state: "ar",
+    country: "us",
     shape: "circle",
     durationMinutes: "5 mins.",
     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
